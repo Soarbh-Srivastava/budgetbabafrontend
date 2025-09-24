1addbe2eb417f67a0b7958ce7445e153ec4dd7ff
Soarbh-Srivastava <22bcs11518@cuchd.in>
2025-09-25 01:38:21 +0530

    case sensitve import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AppContextProvider } from "./context/appContext.jsx";
+import { AppContextProvider } from "./context/AppContext";
 
 createRoot(document.getElementById("root")).render(
   <AppContextProvider>
